7d1db805592bc3bf5c85b5dd4e11d7dd56c01eac
Jhon-Alv <jhon1_alvarado@outlook.com>
2025-06-12 17:31:00 -0500

    add actions for each task-card

--- src/app/task/task-card/task-card.component.ts
+++ src/app/task/task-card/task-card.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { MatCard, MatCardContent, MatCardFooter, MatCardHeader, MatCardTitle } from '@angular/material/card'
 import { Task } from '../../model/Task';
 import { DatePipe } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatMiniFabButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatCardFooter, DatePipe],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatCardFooter, MatIcon, DatePipe, MatMiniFabButton],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
 })
@@ -15,5 +17,15 @@ export class TaskCardComponent {
   @Input() task?: Task;
 
   public taskDate: string = '';
+
+  editTask(){
+    console.log('Edit task with id:', this.task?.id);
+    // Logic to edit the task goes here
+  }
+
+  deleteTask(){
+    console.log('Delete task with id:', this.task?.id);
+    // Logic to delete the task goes here
+  }
 
 }
