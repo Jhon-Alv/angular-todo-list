0749679535335a4c839f28e6cf24fc615b83cc9b
Jhon-Alv <jhon1_alvarado@outlook.com>
2025-06-13 16:28:43 -0500

    upgrade delete task feature with dialogs

---
+++ src/app/dialog/dialog-delete/dialog-delete.component.ts
@@ -0,0 +1,46 @@
+import { Component, Inject, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { TasksService } from '../../services/tasks.service';
+
+@Component({
+  selector: 'app-dialog-delete',
+  standalone: true,
+  imports: [MatDialogContent, MatDialogActions, MatButton, MatDialogClose, MatDialogTitle],
+  templateUrl: './dialog-delete.component.html',
+  styleUrl: './dialog-delete.component.scss'
+})
+export class DialogDeleteComponent {
+  private _dialogRef = inject(MatDialogRef<DialogDeleteComponent>);
+  private _taskService = inject(TasksService);
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: {
+      taskId: number,
+      deleteTask: () => void;
+    }
+  ) {
+
+  }
+
+
+  deleteTask(): void {
+    const taskId: number = this.data.taskId;
+
+    if (!taskId) return;
+
+    this._taskService.deleteTask(taskId).subscribe({
+      next: () => {
+        console.log('Task deleted successfully');
+        this.data.deleteTask();
+        this._dialogRef.close();
+      },
+      error: (error) => {
+        console.error('Error deleting task:', error);
+      }
+    });
+
+  }
+
+
+}
--- src/app/task/task-card/task-card.component.ts
+++ src/app/task/task-card/task-card.component.ts
@@ -3,43 +3,52 @@ import { MatCard, MatCardContent, MatCardFooter, MatCardHeader, MatCardTitle } f
 import { Task } from '../../model/Task';
 import { DatePipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
-import { MatMiniFabButton } from '@angular/material/button';
-import { TasksService } from '../../services/tasks.service';
+import { MatButtonModule, MatMiniFabButton } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogDeleteComponent } from '../../dialog/dialog-delete/dialog-delete.component';
 
 @Component({
   selector: 'app-task-card',
   standalone: true,
-  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatCardFooter, MatIcon, DatePipe, MatMiniFabButton],
+  imports: [MatCard, MatCardHeader, MatCardTitle, MatCardContent, MatCardFooter, MatIcon, DatePipe, MatMiniFabButton, MatButtonModule],
   templateUrl: './task-card.component.html',
   styleUrl: './task-card.component.scss'
 })
 export class TaskCardComponent {
 
-  _taskService = inject(TasksService);
+  readonly _dialogRef = inject(MatDialog);
 
   @Input() task?: Task;
   @Output() taskDeleted = new EventEmitter<void>();
 
-  public taskDate: string = '';
-
-  editTask(){
+  editTask() {
     console.log('Edit task with id:', this.task?.id);
     // Logic to edit the task goes here
   }
 
-  deleteTask(): void{
-
+  openDeleteDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
     if (!this.task?.id) return;
-    this._taskService.deleteTask(this.task.id).subscribe({
-      next: () => {
-        console.log('Task deleted successfully');
-        this.taskDeleted.emit();
-      },
-      error: (error) => {
-        console.error('Error deleting task:', error);
+    this._dialogRef.open(DialogDeleteComponent, {
+      width: '250px',
+      enterAnimationDuration,
+      exitAnimationDuration,
+      data: {
+        taskId: this.task.id,
+        deleteTask: () => {
+          this.taskDeleted.emit();
+        }
       }
     });
 
+    /*
+    this._dialogRef.afterAllClosed.subscribe(res => {
+      console.log('Dialog closed with result:', res);
+      this.taskDeleted.emit();
+    });
+    */
   }
 
+  
+
 }
+
